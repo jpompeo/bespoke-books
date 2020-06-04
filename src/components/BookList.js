a8d938ff18d9064feb72762e61eba11c8abee0b2
Pierre Lourens <plourens3@gmail.com>
2020-06-04 16:58:18 -0400

    Adding hardcoded data to booklist

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -11,27 +11,50 @@ const BookList = () => {
         <Row className="noGutters">
           <ul className="list-covers">
             <li className="book-container" data>
-              <img src="https://m.media-amazon.com/images/I/51TA3VfN8RL.jpg" />
+              <a href="#">
+                <img src="https://m.media-amazon.com/images/I/51TA3VfN8RL.jpg" />
+                <div class="book-thumb-info">
+                  <h1>Harry Potter and the Chamber of Secrets</h1>
+                  <p class="short-description">
+                    In Harry Potter and the Chamber of Secrets, the summer after
+                    Harry's first year at Hogwarts has been his worst summer
+                    ever ... the Dursleys more distant and horrible than ever
+                    before
+                  </p>
+                </div>
+              </a>
             </li>
             <li className="book-container">
-              <img src="https://images-na.ssl-images-amazon.com/images/I/81-y8fgFTTL.jpg" />
+              <a href="#">
+                <img src="https://images-na.ssl-images-amazon.com/images/I/81-y8fgFTTL.jpg" />
+              </a>
             </li>
             <li className="book-container">
-              <img src="https://images-na.ssl-images-amazon.com/images/I/81wLeKWJmrL.jpg" />
+              <a href="#">
+                <img src="https://images-na.ssl-images-amazon.com/images/I/81wLeKWJmrL.jpg" />
+              </a>
             </li>
             <li className="book-container">
-              <img src="https://images-na.ssl-images-amazon.com/images/I/71yP-dPa0mL.jpg" />
+              <a href="#">
+                <img src="https://images-na.ssl-images-amazon.com/images/I/71yP-dPa0mL.jpg" />
+              </a>
             </li>
 
             <li className="book-container">
-              <img src="https://images-na.ssl-images-amazon.com/images/I/81wLeKWJmrL.jpg" />
+              <a href="#">
+                <img src="https://images-na.ssl-images-amazon.com/images/I/81wLeKWJmrL.jpg" />
+              </a>
             </li>
 
             <li className="book-container">
-              <img src="https://images-na.ssl-images-amazon.com/images/I/81-y8fgFTTL.jpg" />
+              <a href="#">
+                <img src="https://images-na.ssl-images-amazon.com/images/I/81-y8fgFTTL.jpg" />
+              </a>
             </li>
             <li className="book-container">
-              <img src="https://images-na.ssl-images-amazon.com/images/I/71yP-dPa0mL.jpg" />
+              <a href="#">
+                <img src="https://images-na.ssl-images-amazon.com/images/I/71yP-dPa0mL.jpg" />
+              </a>
             </li>
           </ul>
         </Row>
