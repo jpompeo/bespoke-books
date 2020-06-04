3e294545a456765788e07ad02742dc7fcaec3de1
Pierre Lourens <plourens3@gmail.com>
2020-06-04 15:57:01 -0400

    Adds basic flex boxes for books

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import '../css/BookList.css';
+import React from "react";
+import "../css/BookList.css";
+import { Row, Col } from "react-bootstrap";
 
 const BookList = () => {
-    return (
-        <div id="book-list">
-            <h1>BookList</h1>
-        </div>
-    )
-}
+  return (
+    <div id="book-list">
+      <h1>BookList</h1>
+
+      <div class="flex-container">
+        <Row className="noGutters">
+          <ul class="list-covers">
+            <li className="book-container">hi</li>
+            <li className="book-container">hi</li>
+            <li className="book-container">hi</li>
+            <li className="book-container">hi</li>
+            <li className="book-container">hi</li>
+            <li className="book-container">hi</li>
+            <li className="book-container">hi</li>
+            <li className="book-container">hi</li>
+            <li className="book-container">hi</li>
+          </ul>
+
+          {/* <Col lg="3">
+            <div className="book-list-thumb"></div>
+          </Col>
+          <Col lg="3">
+            <div className="book-list-thumb"></div>
+          </Col>
+          <Col lg="3">
+            <div className="book-list-thumb"></div>
+          </Col>
+          <Col lg="3">
+            <div className="book-list-thumb"></div>
+          </Col>
+          <Col lg="3">
+            <div className="book-list-thumb"></div>
+          </Col>
+          <Col lg="3">
+            <div className="book-list-thumb"></div>
+          </Col>
+          <Col lg="3">
+            <div className="book-list-thumb"></div>
+          </Col> */}
+        </Row>
+      </div>
+    </div>
+  );
+};
 
 export default BookList;
