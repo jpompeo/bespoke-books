9dc41310c56b2bd77a7e5fab5930e4b81e9b83ad
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-06-04 16:49:17 -0400

    Formatting and styling sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,81 @@
 import React from 'react';
 import '../css/Sidebar.css';
+import _ from 'lodash';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap'
 
 const Sidebar = () => {
+
+
+    // sample list of tags ** TODO replace with state from store
+    const tags = [
+        "Adventure",
+        "Belief",
+        "Forgiveness",
+        "Decisions",
+        "Death & Dying",
+        "Love",
+        "Acceptance",
+        "Courage",
+        "Change",
+        "Empathy",
+        "Overcoming Adversity",
+        "Pressure",
+        "Friendship",
+        "Sacrifice",
+        "The Bonds of Family",
+        "Suffering",
+        "Conflict",
+        "Abandonment",
+        "Alienation",
+        "Ambition",
+        "Coming of Age",
+        "Freedom",
+        "Gender",
+        "Justice",
+        "Isolation",
+        "Cruelty",
+        "Fate",
+        "Hope",
+        "Guilt",
+        "Black Lives Matter",
+        "LGBTQ Pride",
+    ]
+
+    //iterate through all tags to create element for each 
+    const listOfTags = tags.map(tag => {
+        return (
+            <li>
+                <Button> {tag} </Button>
+            </li>
+        )
+    });
+
     return (
-        <div id="sidebar">
-            <h2>Sidebar</h2>
-        </div>
+        <Container fluid id="sidebar">
+            <Row>
+                <Col>
+
+                    {/* Search Bar  */}
+                    <Form>
+                        <Form.Group controlId="searchBooks">
+                            <Form.Label>Search by Book</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Book Title or Author" />
+                        </Form.Group>
+                    </Form>
+
+                    <p>or</p>
+
+                    {/* Tag List */}
+                    <h3>Select a Theme:</h3>
+
+                    <ul>
+                        {/* //mapped list of tags from state  */}
+                        {listOfTags}
+                    </ul>
+                </Col>
+            </Row>
+        </Container>
     )
-    
 }
 
 export default Sidebar;
