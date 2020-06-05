566274ec27ea7bcf692a31512e2405cb3b5d5127
Pierre Lourens <plourens3@gmail.com>
2020-06-05 14:21:19 -0400

    Can add a recommendation... with some limitations

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,8 @@ const ROOT_URL = `https://www.googleapis.com/books/v1/volumes`;
 export const SEARCH_BOOKS = "SEARCH_BOOKS";
 export const SEARCH_BOOK = "SEARCH_BOOK";
 export const SEND_TAGS = "SEND_TAGS";
+export const SEND_RECOMMENDATION = "SEND_RECOMMENDATION";
+export const STORE_RECOMMENDATION = "STORE_RECOMMENDATION";
 
 export function searchBooks(term) {
   const url = `?${ROOT_URL}&q=${term}&maxResults=25`;
@@ -34,6 +36,32 @@ export function searchBook(id) {
 
   return {
     type: SEARCH_BOOK,
+    payload: request,
+  };
+}
+
+export function sendRecommendation(values, id) {
+  const request = values;
+
+  alert(id);
+
+  return {
+    type: SEND_RECOMMENDATION,
+    payload: request,
+  };
+}
+
+export function storeRecommendation(formState, id, image) {
+  const request = {
+    user: formState.user,
+    text: formState.text,
+    id: id,
+    image: image,
+    tags: formState.tags,
+  };
+
+  return {
+    type: STORE_RECOMMENDATION,
     payload: request,
   };
 }
