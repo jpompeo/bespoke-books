1fb4e4e536418672de86935c91b4085c3dc1a458
Pierre Lourens <plourens3@gmail.com>
2020-06-04 23:52:37 -0400

    prep for pull

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,16 +3,16 @@ import axios from "axios";
 // change to google books api
 const ROOT_URL = `https://www.googleapis.com/books/v1/volumes?`;
 
-export const SEARCH_BOOK = "SEARCH_BOOK";
+export const SEARCH_BOOKS = "SEARCH_BOOKS";
 
-export function searchBook(term) {
+export function searchBooks(term) {
   const url = `${ROOT_URL}&q=${term}`;
   const request = axios.get(url);
 
   console.log("Request", request);
 
   return {
-    type: SEARCH_BOOK,
+    type: SEARCH_BOOKS,
     payload: request,
   };
 }
--- src/reducers/books-reducer.js
+++ src/reducers/books-reducer.js
@@ -1,10 +1,10 @@
-import { SEARCH_BOOKS } from '../actions/index';
+import { SEARCH_BOOKS } from "../actions/index";
 
-export default function(state = [], action) {
-    switch (action.type) {
-        case SEARCH_BOOKS:
-            return [action.payload.data, ...state];
-        default: 
-            return state;
-    }
+export default function (state = [], action) {
+  switch (action.type) {
+    case SEARCH_BOOKS:
+      return [action.payload.data, ...state];
+    default:
+      return state;
+  }
 }
