3f6ed592c960fd4aa226f10cef8229a3aa70c0ec
Pierre Lourens <plourens3@gmail.com>
2020-06-05 02:43:47 -0400

    Removes annoying alert

--- src/reducers/chosenTagsReducer.js
+++ src/reducers/chosenTagsReducer.js
@@ -3,7 +3,6 @@ import { SEND_TAGS } from "../actions/index";
 export default function (state = "", action) {
   switch (action.type) {
     case SEND_TAGS:
-      alert(action.payload);
       return action.payload;
     default:
       return state;
